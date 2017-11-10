b0bbf437011137ba167b3e5fed456eaee30c48d4
Jittat Fakcharoenphol <jittat@gmail.com>
2017-11-11 05:40:17 +0700

    counts input not greater than m

--- max/js/custom_blocks.js
+++ max/js/custom_blocks.js
@@ -151,3 +151,21 @@ Blockly.JavaScript['variables_set'] = function(block) {
       'highlightVariableSet("' + varName + '",' + varName + ');\n';
   }
 };
+
+
+Blockly.JavaScript['math_change'] = function(block) {
+  // Add to a variable in place.
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'DELTA',
+      Blockly.JavaScript.ORDER_ADDITION) || '0';
+  var varName = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
+
+  if(!MaxCustomBlock.HIGHLIGHT_VARIABLE_SET) {
+    return varName + ' = (typeof ' + varName + ' == \'number\' ? ' + varName +
+      ' : 0) + ' + argument0 + ';\n';
+  } else {
+    return varName + ' = (typeof ' + varName + ' == \'number\' ? ' + varName +
+      ' : 0) + ' + argument0 + ';\n' +
+      'highlightVariableSet("' + varName + '",' + varName + ');\n';
+  }
+};
